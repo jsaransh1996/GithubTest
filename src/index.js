9b0534e5c7bb1c7477f8bda274b25e5685fef565
Saransh Jain <saransh.jain@flipkart.com>
2021-09-30 02:12:57 +0530

    Shortened the reach path

--- src/index.js
+++ src/index.js
@@ -3,30 +3,39 @@ const fetch = require("node-fetch");
 // let host = "https://api.github.com";
 let args = process.argv.slice(2);
 let host = args[0];
-console.log(host);
 
 let org = "Flipkart";
 let repo = "fk-ios-app";
-let branchesURL = `${host}/repos/${org}/${repo}/branches`;
+let masterURL = `${host}/repos/${org}/${repo}/branches/master`;
 
-getReponse(branchesURL).then((branches) => {
-  console.log(branches);
-  let master = branches.filter((branch) => branch["name"] === "master")[0];
-  let latestCommit = master["commit"]["sha"];
+getReponse(masterURL).then((masterData) => {
+  console.log(masterData);
+  let message = masterData["commit"]["commit"]["mesage"];
+  let author = masterData["commit"]["commit"]["author"]["name"];
+  console.log(message);
 
-  let commitURL = `${host}/repos/${org}/${repo}/git/commits/${latestCommit}`;
-
-  getReponse(commitURL).then((commitDetails) => {
-    console.log(commitDetails);
-    let message = commitDetails["message"];
-    let authorName = commitDetails["author"]["name"];
-    var proceed = true;
-    if (message.startsWith("Bumped app version") && authorName == "ghe-admin") {
-      proceed = false;
-      console.log(proceed);
-    }
+  let message = commitDetails["message"];
+  let authorName = commitDetails["author"]["name"];
+  var proceed = true;
+  if (message.startsWith("Bumped app version") && authorName == "ghe-admin") {
+    proceed = false;
     console.log(proceed);
-  });
+  }
+  console.log(proceed);
+
+  // let latestCommit = masterData["commit"]["sha"];
+  // let commitURL = `${host}/repos/${org}/${repo}/git/commits/${latestCommit}`;
+  // getReponse(commitURL).then((commitDetails) => {
+  //   console.log(commitDetails);
+  //   let message = commitDetails["message"];
+  //   let authorName = commitDetails["author"]["name"];
+  //   var proceed = true;
+  //   if (message.startsWith("Bumped app version") && authorName == "ghe-admin") {
+  //     proceed = false;
+  //     console.log(proceed);
+  //   }
+  //   console.log(proceed);
+  // });
 });
 
 function getReponse(url) {
