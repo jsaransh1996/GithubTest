e723cc4ad486851a6f9f54fd71be25249c64b45c
Saransh Jain <saransh.jain@flipkart.com>
2021-09-30 03:29:26 +0530

    removed logs

--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ let host = args[0];
 let org = "Flipkart";
 let repo = "fk-ios-app";
 let masterURL = `${host}/repos/${org}/${repo}/branches/master`;
-console.log(masterURL);
+// console.log(masterURL);
 
 request.get(masterURL, (error, response, body) => {
   let json = JSON.parse(body);
-  console.log(json);
+  // console.log(json);
   let message = json.commit.commit.message;
   let authorName = json.commit.commit.author.name;
-  console.log(message);
-  console.log(authorName);
+  // console.log(message);
+  // console.log(authorName);
 
   var proceed = true;
   if (message.startsWith("Bumped app version") && authorName == "Jenkins") {
     proceed = false;
-    console.log(proceed);
   }
   console.log(proceed);
 });
