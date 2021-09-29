03bea8023a1b20528d9fc67ccf4162f40a844d9e
Saransh Jain <saransh.jain@flipkart.com>
2021-09-30 03:18:41 +0530

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const fetch = require("node-fetch");
 const request = require("request");
 
-// let host = "https://api.github.com";
 let args = process.argv.slice(2);
 let host = args[0];
 
@@ -13,54 +12,15 @@ console.log(masterURL);
 request.get(masterURL, (error, response, body) => {
   let json = JSON.parse(body);
   console.log(json);
-  console.log(json.commit.commit.message);
-  console.log(json.commit.commit.author);
+  let message = json.commit.commit.message;
+  let author = json.commit.commit.author.name;
+  console.log(message);
+  console.log(author);
+
+  var proceed = true;
+  if (message.startsWith("Bumped app version") && authorName == "Jenkins") {
+    proceed = false;
+    console.log(proceed);
+  }
+  console.log(proceed);
 });
-
-// getReponse(masterURL).then((masterData) => {
-//   console.log(masterData);
-// });
-// getReponse(masterURL).then((masterData) => {
-//   console.log(masterData);
-//   // let message = masterData["commit"]["commit"]["mesage"];
-//   // let authorName = masterData["commit"]["commit"]["author"]["name"];
-//   // console.log(message);
-
-//   // var proceed = true;
-//   // if (message.startsWith("Bumped app version") && authorName == "ghe-admin") {
-//   //   proceed = false;
-//   //   console.log(proceed);
-//   // }
-//   // console.log(proceed);
-
-//   // let latestCommit = masterData["commit"]["sha"];
-//   // let commitURL = `${host}/repos/${org}/${repo}/git/commits/${latestCommit}`;
-//   // getReponse(commitURL).then((commitDetails) => {
-//   //   console.log(commitDetails);
-//   //   let message = commitDetails["message"];
-//   //   let authorName = commitDetails["author"]["name"];
-//   //   var proceed = true;
-//   //   if (message.startsWith("Bumped app version") && authorName == "ghe-admin") {
-//   //     proceed = false;
-//   //     console.log(proceed);
-//   //   }
-//   //   console.log(proceed);
-//   // });
-// });
-
-function getReponse(url) {
-  return fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("Data");
-      console.log(data);
-      console.log(data["commit"]);
-      let message = data["commit"]["commit"]["mesage"];
-      console.log(message);
-
-      Promise.resolve(message);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
