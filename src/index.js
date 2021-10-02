9266d211dcef89b7d69deca6b73953aaca3e538d
Saransh Jain <saransh.jain@flipkart.com>
2021-10-02 05:51:17 +0530

    node-test

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const request = require("request");
+import fetch from "node-fetch";
 
 let args = process.argv.slice(2);
 let host = args[0];
-
 let org = "Flipkart";
 let repo = "fk-ios-app";
-let masterURL = `${host}/repos/${org}/${repo}/branches/master`;
+// let host = "https://api.github.com";
+// let org = "chandrakiranks";
+// let repo = "git-util";
+// let masterURL = `${host}/repos/${org}/${repo}/branches/master`;
 // console.log(masterURL);
 
-request.get(masterURL, (error, response, body) => {
-  let json = JSON.parse(body);
-  // console.log(json);
-  let message = json.commit.commit.message;
-  let authorName = json.commit.commit.author.name;
-  // console.log(message);
-  // console.log(authorName);
+let masterData = await fetch(masterURL);
+let json = await masterData.json();
+let message = json["commit"]["commit"]["message"];
+let authorName = json["commit"]["commit"]["author"]["name"];
+// console.log(message);
+// console.log(authorName);
 
-  var proceed = true;
-  if (message.startsWith("Bumped app version") && authorName == "Jenkins") {
-    proceed = false;
-  }
-  console.log(proceed);
-});
+var proceed = true;
+if (message.startsWith("Bumped app version") && authorName == "Jenkins") {
+  proceed = false;
+}
+console.log(proceed);
